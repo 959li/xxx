dcfe1ace82d12904cb4c607a42b5b5b127b950b9
李家路 <103433145+959li@users.noreply.github.com>
2022-05-05 22:03:45 +0800

    Add files via upload

---
+++ 计算器极简.cpp
@@ -0,0 +1,85 @@
+#include<stdio.h>
+
+
+int jia(int x, int y)
+{
+	return (x + y);
+}
+
+
+int jian(int x, int y)
+{
+	return (x - y);
+}
+
+
+int cheng(int x, int y)
+{
+	return (x * y);
+}
+
+
+int chu(int x, int y)
+{
+	return (x / y);
+}
+
+void menu()
+{
+	printf("*****************************\n");
+	printf("****** 1：加法  2：减法 ******\n");
+	printf("****** 3：乘法  4：除法 ******\n");
+	printf("*******   0：退出程序   ******\n");
+	printf("*****************************\n");
+
+}
+
+int main()
+{
+	int input = 0;
+	
+	do
+	{
+		
+		int x = 0;
+		int y = 0;
+		menu();
+		int ret = 0;
+		printf("请输入指令>");
+		scanf_s("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("请输入操作数：");
+			scanf_s("%d %d", &x, &y);
+			ret=jia(x,y);
+			printf("%d\n", ret);
+			break;
+		case 2:
+			printf("请输入操作数：");
+			scanf_s("%d %d", &x, &y);
+			ret = jian(x,y);
+			printf("%d\n", ret);
+			break;
+		case 3:
+			printf("请输入操作数：");
+			scanf_s("%d %d", &x, &y);
+			ret = cheng(x, y);
+			printf("%d\n", ret);
+			break;
+		case 4:
+			printf("请输入操作数：");
+			scanf_s("%d %d", &x, &y);
+			ret = chu(x, y);
+			printf("%d\n", ret);
+			break;
+		case 0:
+			printf("退出程序");
+			break;
+		default:
+			printf("输入错误，请重新输入：/n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
