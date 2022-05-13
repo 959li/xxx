f877952c91d8f7470e193bd2ac5e10b5d3e0f7f4
李家路 <103433145+959li@users.noreply.github.com>
2022-05-13 21:18:57 +0800

    Add files via upload

---
+++ text323.cpp
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main() {
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+
+	int age = 20;
+	double weight = 75.3f;
+
+	///*
+	scanf("%d %d", &a, &b);
+	printf("a + b = %d\n", a+b);
+	return 0;
+}
