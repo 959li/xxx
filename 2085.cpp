2a9f6f5a743668be8c2cd014bed0bc654a6c8d96
李家路 <103433145+959li@users.noreply.github.com>
2022-05-17 22:13:17 +0800

    Add files via upload

--- 2085.cpp
+++ 2085.cpp
@@ -1,69 +1,1 @@
-#include<stdio.h>
-#include<string.h>
-
-
-int bijiao(const char* s1, const char* s2[], int sz2)
-{
-	int i = 0;
-	int q = 0;
-	for (i = 0; i < sz2; i++)
-	{
-		if (strcmp(s1, s2[i]) == 0)
-		{
-			q++;
-		}
-
-	}
-	return q;
-}
-int chazhao(const char* s1[], int i, int sz)
-{
-	int j = 0;
-	int count = 0;
-	for (j = 0; j < sz; j++)
-	{
-		if (s1[i] == s1[j])
-			count++;
-	}
-	return count;
-}
-int countword(const char* s1[], int sz1, const char* s2[], int sz2)
-{
-	int i = 0;
-	int z = 0;
-
-	for (i = 0; i < sz1; i++)
-	{
-
-		if (chazhao(s1, i, sz1) == 1)
-		{
-			if (bijiao(s1[i], s2, sz2) == 1)
-			{
-				z++;
-			}
-		}
-
-
-
-	}
-	return z;
-}
-
-int main()
-{
-
-	const char* s1[5] = { 0 };
-	s1[0] = "b";
-	s1[1] = "bb";
-	s1[2] = "amazingb";
-	s1[3] = "as";
-	s1[4] = "is";
-	int sz1 = sizeof(s1) / sizeof(s1[0]);
-	const char* s2[2] = { 0 };
-	s2[0] = "a";
-	s2[1] = "i";
-	int sz2 = sizeof(s2) / sizeof(s2[0]);
-	int ans = countword(s1, sz1, s2, sz2);
-	printf("%d ", ans);
-
-}
+#include<stdio.h>#include<string.h>#include<malloc.h>char** delete1(const char** num, int numSize){	int i = 0;	int j = 0;	const char** s1 = (char**)malloc(sizeof(char*) * numSize);	int u = 0;	while (i < numSize)	{		j = 0;		while (j < numSize)		{			if (strcmp(num[i], num[j]) == 0 && i != j)  //判断两个字符串是否相等，相等为零，不相等不为0，strcmp函数的作用			{				break;			}			j++;		}		if (j == numSize)		{			s1[u++] = &num[i][0]; //每一个指针指向一个首地址		}		i++;	}	while (u < numSize)	{		s1[u++] = NULL; //指针赋空	}	int mm = 0;	while (mm < u)	{		printf("%s\n", s1[mm]); //输出字符串的内容		mm++;	}	return (char**)s1;}int countWords(const char** words1, int words1Size, char** words2, int words2Size){	//char *(l1[3])=delete1(words1,words1Size);	words1 = delete1(words1, words1Size);	words2 = delete1(words2, words2Size);	int i, j;	i = 0;	j = 0;	printf("%d\n", i);	while (words1[i])	{		i++;	}	while (words2[j])	{		j++;	}	int counter = 0;	int m, n;	m = 0;	n = 0;	while (m < i)	{		n = 0;		while (n < j)		{			if (!strcmp(words1[m], words2[n]))			{				counter++;				break;			}			n++;		}		m++;	}	return counter;}int main(){	const char** s1 = (char**)malloc(sizeof(char*) * 5);	s1[0] = "leetcode";	s1[1] = "is";	s1[2] = "amazing";	s1[3] = "as";	s1[4] = "is";	char** s2 = (char**)malloc(sizeof(char*) * 3);	s1[0] = "amazing";	s1[1] = "leetcode";	s1[2] = "is";	printf("%d", countWords(s1, 5, s2, 3));	return 0;}
