691dfc920718d1c942bb7c5361018898e3c34d20
李家路 <103433145+959li@users.noreply.github.com>
2022-06-13 22:20:05 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,54 +1,22 @@
 #include<stdio.h>
-//int main() {
-//
-//	int a = 10;
-//	printf("%p\n", &a);
-//	int* pa = &a;
-//	*pa = 100;
-//	printf("%d\n", a);
-//	return 0;
-//}
-//int main() 
-//{
-//
-//	printf("%d\n", sizeof(char*));
-//	printf("%d\n", sizeof(short*));
-//	printf("%d\n", sizeof(int*));
-//	printf("%d\n", sizeof(long*));
-//	printf("%d\n", sizeof(long long*));
-//	printf("%d\n", sizeof(float*));
-//	
-//	return 0;
-//}
-//int max(int a, int b) {
-//	if (a > b)
-//		return a;
-//	else
-//	{
-//		return b;
-//	}
-//}
-//int main() {
-//
-//	int c = max(2, 3);
-//	printf("%d\n", c);
-//
-//	return 0;
-//
-//}
 int main() {
-	int j = 0;
-	for (int i = 0; i <= 100; i++) {
-		if (i % 2 == 0) {
 
-		}
-		else
+	int i = 0;
+	int n = 1;
+	int ret = 1;
+	int wum = 0;
+	while (n<=10)
+	{
+		for (i = 1; i <= n; i++)
 		{
-			printf("%d\n", i);
-			j++;
+			ret = ret * i;
 		}
-	}
-	printf("%d\n", j);
+		n++;
+		wum = wum + ret;
+		ret = 1;
 
+	}
+	printf("%d", wum);
+	printf("%d\n", ret);
 	return 0;
 }
